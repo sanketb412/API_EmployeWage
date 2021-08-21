ab55dc9d1b3e193d348d9be74c33ac9945bb5f6e
sanketb412 <sanketbagde38@gmail.com>
2021-08-21 15:44:32 +0530

    [Sanket B] Add: code for getting All Employee Detials through GET API

--- REST_EmpWage.py
+++ REST_EmpWage.py
@@ -10,8 +10,46 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route("/",methods=['GET'])
-def get_emp_ID():
+def get_message():
     return jsonify({'Message': 'Welcome to Employee-Wage'})
+
+
+'''
+Defining the data for referance purpose.
+Having 3 Employee Data in JSON format.
+'''
+Emp_details = [
+    {
+        'ID': 1,
+        'Employee_Name': 'Sanket',
+        'Employee_ID': 345,
+        'Employee_Daily_Wage': 400
+    },
+    {
+        'ID': 2,
+        'Employee_Name': 'Nikita',
+        'Employee_ID': 322,
+        'Employee_Daily_Wage': 500
+    },
+    {
+        'ID': 3,
+        'Employee_Name': 'Anusha',
+        'Employee_ID': 3452,
+        'Employee_Daily_Wage': 600
+    }
+]
+
+@app.route("/employee", methods=['GET'])
+def get():
+    """
+        Description:
+            function get call the data to fet all the entries
+        Parameter:
+            just to difne the object Employee Parameter is define.
+        Return:
+            returning the Json object.
+    """    
+    return jsonify({'Employee':Emp_details})
 
 if __name__ == '__main__':
     # calling main function to run the app while debug is to be True
